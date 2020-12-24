8744834381bd13c2447dddbdccf78fe5cdfc492c
Majd Hasan <majd.hasan@aperto.com>
2020-12-24 16:34:12 +0100

    add transaction schema and model

--- server/src/models/transaction.model.js
+++ server/src/models/transaction.model.js
@@ -1,7 +1,43 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const TransactionSchema = Schema({});
+const TransactionSchema = Schema({
+  amount: { type: Number, require: true },
+  order: { type: Schema.Types.ObjectId, ref: 'Order' },
+  issuer: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    refPath: 'transactionSide',
+  },
+  receiver: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    refPath: 'transactionSide',
+  },
+  issuerModel: {
+    type: String,
+    required: true,
+    enum: ['Shop', 'Customer'],
+  },
+  receiverModel: {
+    type: String,
+    required: true,
+    enum: ['Shop', 'Customer'],
+  },
+  /**
+   * Type: charge, payment, reimbursement
+   */
+
+  type: { type: String, require: true },
+
+  /**
+   * Status: pending ,issued, failed
+   */
+
+  status: { type: String, default: 'pending' },
+  issuedOn: { type: Date, default: new Date() },
+  closedOn: { type: Date },
+});
 
 const Transaction = mongoose.model('Transaction', TransactionSchema);
 module.exports = Transaction;
