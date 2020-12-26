1bf44010a45d85e8bf54ac7461d9d84bee7fa37d
Majd Hasan <majd.hasan@aperto.com>
2020-12-26 21:59:18 +0100

    add order creation

--- server/src/controllers/order.controller.js
+++ server/src/controllers/order.controller.js
@@ -1,18 +1,41 @@
+const Customer = require('../models/customer.model');
 const Order = require('../models/order.model');
+const Shop = require('../models/shop.model');
 
 const orderController = {};
 
 orderController.create = async (req, res, next) => {
-  const { description, amount, customer } = req.body;
+  const { description, amount, customerId } = req.body;
+  const shopId = req.shop._id;
+
   const newOrder = new Order({
     description,
     amount,
-    shop: req.shop._id,
-    customer: customer,
+    openAmount: amount,
+    shop: shopId,
+    customer: customerId,
   });
   try {
-    const order = await newOrder.save();
-    return res.send({ order });
+    const customer = await Customer.findOne({ _id: customerId });
+    const shop = await Shop.findOne({ _id: shopId });
+    console.log('customer', customer);
+    console.log('shop', shop);
+
+    if (customer && shop) {
+      const order = await newOrder.save();
+      customer.orders.push(order);
+      shop.orders.push(order);
+      await customer.save();
+      await shop.save();
+
+      return res.send({ order });
+    } else {
+      const err = new Error(
+        `The Customer with id ${customerId} was not found in our system`,
+      );
+      err.status = 404;
+      return next(err);
+    }
   } catch (e) {
     next(e);
   }
