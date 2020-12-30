e0f8b5dda2ad189b3784a114a9785d6f6760ef9d
Majd Hasan <majd.hasan@aperto.com>
2020-12-31 00:20:34 +0100

    prepare redux to shop

---
+++ shop/src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import auth from './auth_reducer';
+import signup from './signup_reducer';
+import transaction from './transaction_reducer';
+import order from './order_reducer';
+import error from './error_reducer';
+
+export default combineReducers({
+  auth,
+  signup,
+  transaction,
+  order,
+  error,
+});
---
+++ shop/src/reducers/order_reducer.js
@@ -0,0 +1,30 @@
+import {
+  ORDER_SAVED,
+  ORDER_RESET,
+  ORDER_FETCHED,
+  ORDER_FETCHING_FAILED,
+  ORDER_FETCHING,
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  saved: false,
+  orders: [],
+  fetching: false,
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ORDER_SAVED:
+      return { ...state, saved: true };
+    case ORDER_RESET:
+      return { ...state, saved: false };
+    case ORDER_FETCHED:
+      return { ...state, fetching: false, orders: action.payload };
+    case ORDER_FETCHING_FAILED:
+      return { ...state, fetching: false };
+    case ORDER_FETCHING:
+      return { ...state, fetching: true };
+    default:
+      return state;
+  }
+};
