7ef457db842dbcacbccc7c3d644ec96d793eb2d9
Majd Hasan <majd.hasan@aperto.com>
2020-12-31 00:22:32 +0100

    prepare shop react boilerplate

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'reactstrap'
+import { Home, Login, Signup } from './pages'
+import { Navigation, ProtectedRoute } from './components'
 
 function App() {
-  return <div></div>;
+  return (
+    <div >
+      <Navigation></Navigation>
+      <Container>
+        <Switch>
+          <ProtectedRoute path="/" component={Home} exact />
+        </Switch>
+        <Route path="/login" component={Login} exact />
+        <Route path="/signup" component={Signup} exact />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
